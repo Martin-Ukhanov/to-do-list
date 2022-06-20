3231ad2704ccd562ad67baa2d00cff8f19945a7d
Martin <martin.ukhanov@gmail.com>
2022-06-19 21:11:03 -0400

    Made progress on adding projects and tasks

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,10 +1,13 @@
 import {toDoList} from "./toDoList";
+import {project} from "./project";
 import {task} from "./task";
+import {parseISO, isToday, isThisWeek} from "date-fns";
 
 const UI = (() => {
     const toggleMenuBtn = document.getElementById('toggle-menu-btn');
     const projectBtns = document.querySelectorAll('#menu nav button.project');
     const addProjectBtn = document.getElementById('add-project-btn');
+    const addProjectForm = document.getElementById('add-project-form');
     const addTaskBtn = document.getElementById('add-task-btn');
     const addTaskForm = document.getElementById('add-task-form');
 
@@ -14,53 +17,41 @@ const UI = (() => {
         const btn = document.createElement('button');
         btn.classList.add('project');
 
-        const projectName = document.createElement('p');
-        projectName.textContent = name;
-
-        btn.appendChild(projectName);
+        btn.innerHTML = `
+            <span class="material-symbols-outlined">checklist</span>
+            <p>${name}</p>`;
 
         return btn;
     }
 
     const createTask = (task) => {
-        const createIconBtn = (iconType) => {
-            const btn = document.createElement('button');
-            btn.classList.add('material-symbols-outlined');
-            btn.textContent = iconType;
-            return btn;
-        };
-    
         const taskContainer = document.createElement('div');
         taskContainer.classList.add('task');
-    
-        // Text
-        const textContainer = document.createElement('div');
-    
-        const projectName = document.createElement('p');
-        projectName.textContent = task.getProject();
-    
-        const name = document.createElement('h2');
-        name.textContent = task.getName();
-    
-        const dueDate = document.createElement('p');
-        dueDate.textContent = task.getDueDate();
-    
-        textContainer.appendChild(projectName);
-        textContainer.appendChild(name);
-        textContainer.appendChild(dueDate);
-    
-        // Nav
-        const nav = document.createElement('nav');
-        
-        const completeBtn = createIconBtn('check_box_outline_blank');
-        
-        nav.appendChild(completeBtn);
-    
-        taskContainer.appendChild(textContainer);
-        taskContainer.appendChild(nav);
+
+        taskContainer.innerHTML += `
+            <div>
+                <p>${task.getProject()}</p>
+                <h2>${task.getName()}</h2>
+                <p>${task.getDueDate()}</p>
+            </div>
+            <button class="material-symbols-outlined">check_box_outline_blank</button>`;
     
         return taskContainer;
     };
+
+    const loadProjectBtns = () => {
+        const container = document.querySelector('nav.custom-projects');
+        container.innerHTML = '';
+        
+        for (const project of toDoList.getProjects()) {
+            const projectName = project.getName();
+
+            if (projectName !== 'All' && projectName !== 'Today' && projectName !== 'This Week') {
+                const btn = createProjectBtn(projectName);
+                container.appendChild(btn);
+            }
+        }
+    }
 
     const loadProject = (name) => {
         const tasksGrid = document.getElementById('tasks-grid');
@@ -77,24 +68,63 @@ const UI = (() => {
             tasksGrid.appendChild(taskCard);
         }
     }
+
+    // Load All project by default
+    loadProject(activeProject);
     
     toggleMenuBtn.addEventListener('click', () => {
         const menu = document.getElementById('menu');
         menu.classList.toggle('active');
     });
 
-    // Load All project by default
-    loadProject(activeProject);
-
     // Load corresponding project
     projectBtns.forEach((projectBtn) => {
         projectBtn.addEventListener('click', () => {
+            const projectName = projectBtn.lastElementChild.textContent;
+
+            if (projectName === 'Today' || projectName === 'This Week') {
+                addTaskBtn.classList.remove('active');
+                addTaskForm.classList.remove('active');
+            } else {
+                addTaskBtn.classList.add('active');
+            }
+
             projectBtns.forEach((projectBtn) => {
                 projectBtn.classList.remove('active');
             });
+
             projectBtn.classList.add('active');
-            loadProject(projectBtn.lastElementChild.textContent);
+
+            loadProject(projectName);
         });
+    });
+
+    addProjectBtn.addEventListener('click', () => {
+        addProjectForm.classList.add('active');
+        addProjectBtn.classList.remove('active');
+    });
+
+    const cancelAddProjectBtn = document.getElementById('cancel-add-project-btn');
+    cancelAddProjectBtn.addEventListener('click', () => {
+        addProjectForm.reset();
+        addProjectForm.classList.remove('active');
+        addProjectBtn.classList.add('active');
+    });
+
+    addProjectForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const projectName = document.getElementById('project-name-input').value;
+
+        const newProject = project(projectName);
+
+        toDoList.addProject(newProject);
+
+        loadProjectBtns();
+
+        addProjectForm.reset();
+        addProjectForm.classList.remove('active');
+        addProjectBtn.classList.add('active');
     });
 
     addTaskBtn.addEventListener('click', () => {
@@ -104,13 +134,13 @@ const UI = (() => {
 
     const cancelAddTaskBtn = document.getElementById('cancel-add-task-btn');
     cancelAddTaskBtn.addEventListener('click', () => {
+        addTaskForm.reset();
         addTaskForm.classList.remove('active');
         addTaskBtn.classList.add('active');
     })
 
     // Add new task from form
     addTaskForm.addEventListener('submit', (e) => {
-        console.log('Hello');
         const name = document.getElementById('name-input').value;
         const dueDate = document.getElementById('due-date-input').value;
 
@@ -122,8 +152,13 @@ const UI = (() => {
         if (projectName === 'All' || projectName === 'Today' || projectName === 'This Week') {
             projectName = 'No Project';
         }
-    
-        project.addTask(task(name, dueDate, projectName));
+
+        const newTask = task(name, dueDate, projectName);
+        
+        project.addTask(newTask);
+
+        if (isToday(parseISO(dueDate))) toDoList.getProject('Today').addTask(newTask);
+        if (isThisWeek(parseISO(dueDate))) toDoList.getProject('This Week').addTask(newTask);
 
         loadProject(activeProject);
 
